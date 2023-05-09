69d2275d0966383fa35de6c6ca5748f0cb7eae95
titayi <kitayimike@gmail.com>
2023-05-09 11:57:36 -0700

    Initial Commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,14 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": 'My Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        num_subs = data.get("data").get("subscribers")
+        return num_subs
     else:
         return 0
